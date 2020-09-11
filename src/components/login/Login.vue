cbfaa2a685f4a2f29c97f5b95b835279a8f32e1c
milohu <milo.hu@ferguson.com>
2020-09-11 18:06:38 +0800

    login form input

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -6,14 +6,55 @@
       :style="{ 'backgroundImage': 'url(' + getBanner()+ ')','background-repeat':'no-repeat','background-size':'cover' }"
     ></div>
     <form class="form">
-      <div class="line">
-        <div>中国大陆</div>
+      <div class="line" @click="selectLocationForm">
+        <div>{{selectLocationObj.label}}</div>
         <div class="icon-small">
           <img src="@/assets/icon/right-707070.svg" />
         </div>
       </div>
-      <div class="line">
-        <div></div>
+      <div v-if="showSelectLocationForm" class="masking">
+        <div class="locationForm">
+          <div class="line" :class="{showTopBorder:showTopBorder}">地区选择</div>
+          <div class="radios" ref="locationForm_radios">
+            <label class="line" v-for="(item, index) in getLocation()" :key="index">
+              <input
+                v-model="location"
+                :value="item.value"
+                type="radio"
+                name="location"
+                @click="selectLocation"
+              />
+              <span>{{item.label}}</span>
+            </label>
+          </div>
+          <div class="cancle">
+            <div class="btn selectdisable" @click="selectLocationForm">取消</div>
+          </div>
+        </div>
+      </div>
+      <div class="line phone">
+        <div class="left">{{selectLocationObj.arecode}}</div>
+        <div class="input">
+          <input
+            v-model="phoneNumber"
+            placeholder="请输入手机号码"
+            required
+            maxlength="13"
+          />
+          <div class="clearbtn icon-small-x" @click="clearPhone">
+            <img src="@/assets/icon/cancle.svg" />
+          </div>
+        </div>
+        <div class="btn right">获取验证码</div>
+      </div>
+      <div class="line verifi">
+        <div class="left">验证码</div>
+        <div class="input">
+          <input placeholder="请输入验证码" required/>
+          <div class="clearbtn icon-small-x" @click="clearPhone">
+            <img src="@/assets/icon/cancle.svg" />
+          </div>
+        </div>
       </div>
     </form>
   </div>
@@ -23,16 +64,75 @@
 import Topbar from "@/components/common/Topbar";
 export default {
   components: {
-    Topbar
+    Topbar,
   },
-  data: function() {
-    return {};
+  data: function () {
+    return {
+      showSelectLocationForm: false,
+      location: "china",
+      showTopBorder: false,
+      phoneNumber: "",
+    };
   },
   methods: {
+    clearPhone() {
+      this.phoneNumber = "";
+    },
     getBanner() {
       return require("@/assets/img/login-background.png");
-    }
-  }
+    },
+    getLocation() {
+      return [
+        {
+          id: 1,
+          label: "中国大陆",
+          arecode: "+86",
+          regex: "[0-9]{13}",
+          value: "china",
+        },
+        {
+          id: 2,
+          label: "美国",
+          arecode: "+116",
+          regex: "[0-9]{13}",
+          value: "us",
+        },
+        {
+          id: 3,
+          label: "英国",
+          arecode: "+113",
+          regex: "[0-9]{13}",
+          value: "uk",
+        },
+      ];
+    },
+    selectLocationForm() {
+      this.showSelectLocationForm = !this.showSelectLocationForm;
+    },
+    selectLocation(e) {
+      this.location = e.target.value;
+      this.selectLocationForm();
+    },
+  },
+  computed: {
+    selectLocationObj() {
+      return this.getLocation().find((e) => e.value === this.location);
+    },
+  },
+  watch: {
+    showSelectLocationForm() {
+      this.$nextTick(() => {
+        if (
+          this.showSelectLocationForm === true &&
+          !this.$refs.locationForm_radios.onscroll
+        ) {
+          this.$refs.locationForm_radios.onscroll = () => {
+            this.showTopBorder = this.$refs.locationForm_radios.scrollTop > 0;
+          };
+        }
+      });
+    },
+  },
 };
 </script>
 
@@ -47,9 +147,99 @@ export default {
 }
 .form .line {
   display: flex;
-  justify-content: space-between;
-  padding: 5px 10px;
+  padding: 2% 4%;
   align-items: center;
   height: 40px;
+}
+.locationForm {
+  height: 100vh;
+  margin: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  border-radius: 3px;
+  padding: 20px;
+}
+.locationForm .radios {
+  overflow-y: scroll;
+}
+.locationForm label.line {
+  justify-content: flex-start;
+}
+.locationForm label input[type="radio"] {
+  margin-right: 20px;
+  width: 20px;
+  height: 20px;
+}
+.locationForm label.line span {
+  overflow: scroll;
+  white-space: nowrap;
+}
+.locationForm label.line span::-webkit-scrollbar {
+  display: none; /* Safari and Chrome */
+}
+.cancle {
+  height: 60px;
+  border-top: 0.5px solid rgb(221, 221, 221);
+  position: absolute;
+  bottom: 0px;
+  right: 10px;
+  left: 10px;
+  background-color: white;
+  padding-right: 20px;
+  color: var(--bilibili-color);
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.cancle .btn {
+  height: 50px;
+  width: 50px;
+  line-height: 50px;
+  text-align: center;
+}
+.cancle .btn:active {
+  background-color: rgb(200, 211, 211);
+}
+.showTopBorder {
+  border-bottom: 0.5px solid rgb(221, 221, 221);
+}
+.phone {
+  display: flex;
+  align-items: center;
+}
+.input input {
+  border: none;
+  font-size: 16px;
+  height: 30px;
+  outline: none;
+  width: 100%;
+}
+.clearbtn {
+  visibility: hidden;
+}
+.input input:valid + .clearbtn {
+  visibility: unset;
+}
+.input {
+  display: flex;
+  align-items: center;
+  justify-self: start;
+}
+.phone .left {
+  flex: 2;
+}
+.phone .right {
+  flex: 3;
+  text-align: end;
+}
+.phone .input {
+  flex: 5;
+}
+.verifi .left {
+  flex: 2;
+}
+.verifi .input {
+  flex: 8;
 }
 </style>
